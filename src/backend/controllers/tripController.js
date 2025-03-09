d9046053510d6256e3f9489cbe6c8b36d45d7ff9
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2025-03-09 13:19:32 -0300

    Added tab icon

--- src/backend/controllers/tripController.js
+++ src/backend/controllers/tripController.js
@@ -50,6 +50,7 @@ const generateHTML = (destino, dias, categorias, trip) => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Viagem para ${safeDestino} | Trip Planner</title>
+        <link rel="icon" type="image/x-icon" href="./includes/imgs/tab-icon.png">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
