e04eafe238eb282401bce1a71090bf9bdcb0c90c
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2025-03-09 12:54:20 -0300

    Included system version number in boot message.

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const logger = require('./src/backend/logger/logger');
 const app = require('./src/backend/api/api');
+const packageJson = require('./package.json');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => logger.info(`Server running on ${port}.`));
+const appVersion = packageJson.version;
+
+app.listen(port, () =>
+  logger.info(`TripPlanner [v${appVersion}] running on port [${port}].`)
+);
